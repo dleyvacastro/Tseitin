ab1bdec59fa4b7d6e3ed4a41efe8cf8533fc98b8
dleyvacastro <dleyvacastro@gmail.com>
2020-11-05 08:09:17 -0500

    Clausulas

--- FNC.py
+++ FNC.py
@@ -50,6 +50,13 @@ def Tseitin(A, letrasProposicionalesA):
     letrasProposicionalesB = [chr(x) for x in range(256, 1200)]
     assert(not bool(set(letrasProposicionalesA) & set(letrasProposicionalesB))), u"¡Hay letras proposicionales en común!"
 
+    pila = []
+    i = -1
+    s = A[0]
+    
+    while len(A) > 0:
+        if (s in letrasProposicionalesA) and (len(pila) != 0) and (pila[-1] == '-') 
+    
     #  IMPLEMENTAR AQUI ALGORITMO TSEITIN
     pass
 
@@ -58,14 +65,36 @@ def Tseitin(A, letrasProposicionalesA):
 # Output: L (lista), lista de literales
 # Se asume que cada literal es un solo caracter
 def Clausula(C):
-
+    L =[]
+    while len(C)>0:
+        s = C[0]
+        if s == '-':
+            L.append(s+C[1])
+            C = C[3:]
+        else:
+            L.append(s)
+            C = C[2:]
+    return L
     #  IMPLEMENTAR AQUI ALGORITMO CLAUSULA
-    pass
+
 
 # Algoritmo para obtencion de forma clausal
 # Input: A (cadena) en notacion inorder en FNC
 # Output: L (lista), lista de listas de literales
 def formaClausal(A):
-
+    L = []
+    i = 0
+    while len(A) > 0:
+        if i >= len(A):
+            L.append(Clausula(A))
+            A = []
+        else:
+            if A[i] == 'Y':
+                L.append(Clausula(A[:i]))
+                A = A [i+1:]
+                i = 0
+            else:
+                i += 1
+    return L
+            
     #  IMPLEMENTAR AQUI ALGORITMO FORMA CLAUSAL
-    pass
