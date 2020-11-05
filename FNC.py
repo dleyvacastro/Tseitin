cb3f04e433f4174ef7c1a1e2137e2a6c34937c49
Jorge Gallego <59119768+Jorge644240@users.noreply.github.com>
2020-11-05 08:28:29 -0500

    Update FNC.py
    Tseitin()

--- FNC.py
+++ FNC.py
@@ -50,15 +50,48 @@ def Tseitin(A, letrasProposicionalesA):
     letrasProposicionalesB = [chr(x) for x in range(256, 1200)]
     assert(not bool(set(letrasProposicionalesA) & set(letrasProposicionalesB))), u"¡Hay letras proposicionales en común!"
 
-    pila = []
-    i = -1
+    conjunciones, pila = [], []
+    count = -1
     s = A[0]
-    
+
     while len(A) > 0:
-        if (s in letrasProposicionalesA) and (len(pila) != 0) and (pila[-1] == '-') 
-    
+        if pila.__len__() != 0 and pila[-1] == '-' and (s in letrasProposicionalesA or s in letrasProposicionalesB):
+            count += 1
+            atomo = letrasProposicionalesB[i]
+            pila = pila[:-1]
+            pila.append(f"{atomo}<-> -{s}")
+            A = A[1:]
+            if len(A) > 0:
+                s = A[0]
+        elif s == ')':
+            w = pila[-1]
+            u = pila[-2]
+            v = pila[-3]
+            pila = pila[:len(pila)-4]
+            count += 1
+            atomo = letrasProposicionalesB[i]
+            conjunciones.append(f"{atomo}<->({vuw})")
+            s = atomo
+        else:
+            pila.append(s)
+            A = A[1:]
+            if len(A) > 0:
+                s = A[0]
+
+    B = ''
+    if i < 0:
+        atomo = pila[-1]
+    else:
+        atomo = letrasProposicionalesB[i]
+
+    for element in conjunciones:
+        fnc = enFNC(element)
+        B += f"Y{fnc}"
+
+    B = atomo + B
+    return B
     #  IMPLEMENTAR AQUI ALGORITMO TSEITIN
-    pass
+    #pass
 
 # Subrutina Clausula para obtener lista de literales
 # Input: C (cadena) una clausula
